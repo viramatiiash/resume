e557624536ae6b4fdfa48f844f7d2ce1516afa2a
viramatiiash <virko1996@gmail.com>
2023-06-20 11:19:32 +0300

    Configure React app for deployment to GitHub Pages

---
+++ src/Resume.jsx
@@ -0,0 +1,373 @@
+import styles from './resume.module.scss';
+import './styles/index.scss';
+
+import vira from './assets/img/avatar.jpg';
+import classNames from 'classnames';
+import { useState } from 'react';
+import {
+  experience,
+  hardSkills,
+  education,
+  softSkills,
+  languages,
+  infosUkr,
+  experienceUkr,
+  softSkillsUkr,
+  languagesUkr,
+  contacts,
+  headings,
+  headingsUkr,
+  educationUkr
+} from './infos/Infos';
+
+
+
+
+  const tabs = {
+    UA: 'UA',
+    ENG: 'ENG',
+  };
+
+  const navList = Object.values(tabs);
+
+const Resume = () => {
+  const [tab, setTab] = useState(tabs.ENG);
+
+  return (
+    <div>
+      {tab === tabs.UA ? (
+        <div className={styles.resume}>
+          <div className={styles.firstContainer}>
+            <div className={classNames(styles.imgContainer)}>
+              <img src={vira} alt='Vira Matiiash' />
+            </div>
+
+            <div className={styles.block}>
+              <h2 className={styles.heading}>{headingsUkr[0]}</h2>
+              <div className={styles.contactsBox}>
+                {contacts.map((contact) => (
+                  <div className={styles.text}>
+                    <a href={contact.href}>
+                      {contact.name}
+                      <span className={styles.contact}>{contact.info}</span>
+                    </a>
+                  </div>
+                ))}
+              </div>
+            </div>
+            <div className={styles.block}>
+              <h2 className={styles.heading}>{headingsUkr[1]}</h2>
+              <div className={styles.hardSkillsBox}>
+                <ul className={styles.description}>
+                  {hardSkills.map((item, index) => (
+                    <li key={`li-${index}`} className={styles.text}>
+                      {item}
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+            <div className={styles.block}>
+              <h2 className={styles.heading}>{headingsUkr[2]}</h2>
+              <div className={styles.softSkillsBox}>
+                <ul className={styles.description}>
+                  {softSkillsUkr.map((item, index) => (
+                    <li key={`li-${index}`} className={styles.text}>
+                      {item}
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+            <div className={styles.block}>
+              <h2 className={styles.heading}>{headingsUkr[3]}</h2>
+              <ul className={styles.description}>
+                {languagesUkr.map((item, index) => (
+                  <li key={`li-${index}`} className={classNames(styles.text)}>
+                    {item}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+          <div className={styles.secondContainer}>
+            <div className={styles.headingContainer}>
+              <div className={styles.nameGroup}>
+                <div className={classNames(styles.imgContainerMobile)}>
+                  <img src={vira} alt='Vira Matiiash' />
+                </div>
+                <div className={styles.headings}>
+                  <h1 className={styles.mainHeading}>
+                    <span className={styles.name}>ВІРА</span> <br />
+                    <span className={styles.surname}>МАТІЯШ</span>
+                  </h1>
+                  <h2 className={styles.littleHeading}>Front-End розробник</h2>
+                </div>
+              </div>
+
+              <nav className={styles.nav}>
+                <ul className={styles.langContainer}>
+                  {navList.map((el, index) => (
+                    <li
+                      key={`li-${index}`}
+                      onClick={() => setTab(el)}
+                      className={classNames(
+                        styles.el,
+                        tab === el && styles.clicked
+                      )}
+                    >
+                      {el}
+                    </li>
+                  ))}
+                  {/* <div className={styles.verticalLine}></div> */}
+                </ul>
+              </nav>
+            </div>
+            <div className={styles.block}>
+              <h2 className={styles.heading}>{headingsUkr[4]}</h2>
+              <div className={styles.description}>
+                Працьовита і цілеспрямована веб-розробниця початкового рівня зі
+                здатністю навчатися та співпрацювати з більш ніж 1 ріком досвіду
+                розробки веб-сайтів. Мотивована розробляти для клієнтів зручні
+                програми.
+              </div>
+            </div>
+            <div className={styles.block}>
+              <h2 className={styles.heading}>{headingsUkr[5]}</h2>
+              {educationUkr.map((info) => (
+                <div className={styles.infoBlock}>
+                  <div
+                    className={classNames(styles.description, styles.education)}
+                  >
+                    <div className={styles.year}>{info.year}</div>
+
+                    <div className={styles.educationPlace}>
+                      {info.place.map((place) => (
+                        <div>{place}</div>
+                      ))}
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </div>
+            <div className={styles.block}>
+              <h2 className={styles.heading}>{headingsUkr[6]}</h2>
+              {experienceUkr.map((info) => (
+                <div className={classNames(styles.description, styles.work)}>
+                  <div className={styles.positionInfo}>
+                    <div className={styles.position}>{info.position}</div>
+                    <div className={styles.place}>{info.place}</div>
+                  </div>
+                  <ul className={styles.descriptionBox}>
+                    {info.description.map((item, index) => (
+                      <li key={`li-${index}`} className={styles.item}>
+                        {item}
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              ))}
+            </div>
+            <div className={styles.block}>
+              <h2 className={styles.heading}>{headingsUkr[7]}</h2>
+              <div className={styles.description}>
+                <ul className={styles.courses}>
+                  <li className={styles.course}>
+                    Softserve IT Academy, серпень 2021 (Фронтенд)
+                  </li>
+                  <li className={styles.course}>
+                    Beetroot Academy, січень 2021 - липень 2022 (Фронтенд)
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div className={styles.block}>
+              <h2 className={styles.heading}>{headingsUkr[8]}</h2>
+              <div className={styles.description}>
+                <div className={styles.project}>
+                  <a
+                    className={styles.link}
+                    href='https://ottohansschool.com.ua/'
+                  >
+                    Website: Otto Hans - Duolanguage School{' '}
+                  </a>
+                  <span className={styles.technologies}>
+                    [React JS, SCSS, Firebase]
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      ) : null}
+
+      {tab === tabs.ENG ? (
+        <div className={styles.resume}>
+          <div className={styles.firstContainer}>
+            <div className={styles.imgContainer}>
+              <img src={vira} alt='Vira Matiiash' />
+            </div>
+
+            <div className={styles.block}>
+              <h2 className={styles.heading}>{headings[0]}</h2>
+              <div className={styles.contactsBox}>
+                {contacts.map((contact) => (
+                  <div className={styles.text}>
+                    <a href={contact.href}>
+                      {contact.name}{' '}
+                      <span className={styles.contact}>{contact.info}</span>
+                    </a>
+                  </div>
+                ))}
+              </div>
+            </div>
+            <div className={styles.block}>
+              <h2 className={styles.heading}>{headings[1]}</h2>
+              <div className={styles.hardSkillsBox}>
+                <ul className={styles.description}>
+                  {hardSkills.map((item, index) => (
+                    <li key={`li-${index}`} className={styles.text}>
+                      {item}
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+            <div className={styles.block}>
+              <h2 className={styles.heading}>{headings[2]}</h2>
+              <div className={styles.softSkillsBox}>
+                <ul className={styles.description}>
+                  {softSkills.map((item, index) => (
+                    <li key={`li-${index}`} className={styles.text}>
+                      {item}
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+            <div className={styles.block}>
+              <h2 className={styles.heading}>{headings[3]}</h2>
+              <ul className={styles.description}>
+                {languages.map((item, index) => (
+                  <li key={`li-${index}`} className={classNames(styles.text)}>
+                    {item}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+          <div className={styles.secondContainer}>
+            <div className={styles.headingContainer}>
+              <div className={styles.nameGroup}>
+                <div className={classNames(styles.imgContainerMobile)}>
+                  <img src={vira} alt='Vira Matiiash' />
+                </div>
+                <div className={styles.headings}>
+                  <h1 className={styles.mainHeading}>
+                    <span className={styles.name}>VIRA</span> <br />
+                    <span className={styles.surname}>MATIIASH</span>
+                  </h1>
+                  <h2 className={styles.littleHeading}>Front-End Developer</h2>
+                </div>
+              </div>
+
+              <nav className={styles.nav}>
+                <ul className={styles.langContainer}>
+                  {navList.map((el, index) => (
+                    <li
+                      key={`li-${index}`}
+                      onClick={() => setTab(el)}
+                      className={classNames(
+                        styles.el,
+                        tab === el && styles.clicked
+                      )}
+                    >
+                      {el}
+                    </li>
+                  ))}
+                  {/* <div className={styles.verticalLine}></div> */}
+                </ul>
+              </nav>
+            </div>
+            <div className={styles.block}>
+              <h2 className={styles.heading}>{headings[4]}</h2>
+              <div className={styles.description}>
+                Hardworking and self-motivated Entry-Level Web Developer with
+                ability to learn and collaborate and more than 1 year experience
+                in maintaining websites. Motivated to provide clients with
+                user-friendly applications.
+              </div>
+            </div>
+            <div className={styles.block}>
+              <h2 className={styles.heading}>{headings[5]}</h2>
+              {education.map((info) => (
+                <div className={styles.infoBlock}>
+                  <div
+                    className={classNames(styles.description, styles.education)}
+                  >
+                    <div className={styles.year}>{info.year}</div>
+
+                    <div className={styles.educationPlace}>
+                      {info.place.map((place) => (
+                        <div>{place}</div>
+                      ))}
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </div>
+            <div className={styles.block}>
+              <h2 className={styles.heading}>{headings[6]}</h2>
+              {experience.map((info) => (
+                <div className={classNames(styles.description, styles.work)}>
+                  <div className={styles.positionInfo}>
+                    <div className={styles.position}>{info.position}</div>
+                    <div className={styles.place}>{info.place}</div>
+                  </div>
+                  <ul className={styles.descriptionBox}>
+                    {info.description.map((item, index) => (
+                      <li key={`li-${index}`} className={styles.item}>
+                        {item}
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              ))}
+            </div>
+            <div className={styles.block}>
+              <h2 className={styles.heading}>{headings[7]}</h2>
+              <div className={styles.description}>
+                <ul className={styles.courses}>
+                  <li className={styles.course}>
+                    Softserve IT Academy, August 2021 (Frontend)
+                  </li>
+                  <li className={styles.course}>
+                    Beetroot Academy, January 2021 - July 2022 (Frontend)
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div className={styles.block}>
+              <h2 className={styles.heading}>{headings[8]}</h2>
+              <div className={styles.description}>
+                <div className={styles.project}>
+                  <a
+                    className={styles.link}
+                    href='https://ottohansschool.com.ua/'
+                  >
+                    Website: Otto Hans - Duolanguage School{' '}
+                  </a>
+                  <span className={styles.technologies}>
+                    [React JS, SCSS, Firebase]
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      ) : null}
+    </div>
+  );
+};
+
+export default Resume;
---
+++ src/infos/Infos.jsx
@@ -0,0 +1,176 @@
+// ! English
+export const education = [
+  {
+    year: 'September 2014 - Mai 2018',
+    place: [
+      'Vasyl Stefanyk Precarpathian National University',
+      'German language and literature',
+      'Bachelor',
+    ],
+  },
+  {
+    year: 'September 2018 - January 2019',
+    place: [
+      'Vasyl Stefanyk Precarpathian National University',
+      'German language and literature',
+      'Master',
+    ],
+  },
+];
+
+export const headings = [
+  'Contacts',
+  'Hard Skills',
+  'Soft Skills',
+  'Languages',
+  'About Me',
+  'Education',
+  'Work Experience',
+  'Courses',
+  'Projects',
+];
+
+export const contacts = [
+  {
+    name: 'E-mail :',
+    href: 'mailto:virko1996@gmail.com',
+    info: 'virko1996@gmail.com',
+  },
+  {
+    name: 'Phone :',
+    href: 'tel:+380992762398',
+    info: '+380 99 276 2398',
+  },
+  {
+    name: 'LinkedIn',
+    href: 'https://www.linkedin.com/in/vira-matiiash-5991a6277',
+    
+  },
+];
+
+export const experience = [
+  {
+    position: 'Front-End Developer',
+    place: 'Otto Hans Language School, Ivano-Frankivsk, 2022 - up to now',
+    description: [
+      'Troubleshoot website problems',
+      'Developed clean code for the client project using React',
+      'Created a Mobile Responsive User Experience that allowed cross-platform accessibility to the website on any device with primary use on mobile',
+      'Reviewed the website to ensure functionality',
+      'Used Firebase Database to allow users to submit information by storing data there',
+    ],
+  },
+  {
+    position: 'Teacher of German',
+    place: 'Otto Hans Language School, Ivano-Frankivsk, June 2020 - up to now',
+    description: [
+      'Taught adult students at A1-C1 levels of German language',
+      'Worked to build and maintain a positive learning environment',
+    ],
+  },
+  {
+    position: 'Tutor',
+    place: 'Independent, 2017 - up to now',
+    description: [
+      'Created lesson plans responsive to individual students’ needs',
+      'Directed learning to achieve student engagement with material',
+    ],
+  },
+];
+
+export const hardSkills = [
+  'HTML',
+  'CSS3/SCSS',
+  'JavaScript',
+  'Pug',
+  'React JS',
+  'Git',
+  'Firebase',
+];
+
+export const softSkills = [
+  'Communicative skills',
+  'Critical thinking',
+  'Attention to details',
+  'Effective Time Management',
+  'Skills for problem solving',
+  'Be creative',
+  'Capacity to work under pressure',
+];
+
+export const languages = ['Ukrainian', 'English', 'German'];
+
+// ! Ukrainian
+export const educationUkr = [
+  {
+    year: 'Вересень 2014 - травень 2018',
+    place: [
+      'Прикарпатський національний університет імені Василя Стефаника',
+      'Німецька мова та література',
+      'Бакалавр',
+    ],
+  },
+  {
+    year: 'Вересень 2018 - січень 2019',
+    place: [
+      'Прикарпатський національний університет імені Василя Стефаника',
+      'Німецька мова та література',
+      'Магістр',
+    ],
+  },
+];
+
+export const experienceUkr = [
+  {
+    position: 'Front-End розробник',
+    place: 'Мовна школа Otto Hans, Івано-Франківськ, 2022 - по теперішній час',
+    description: [
+      'Вирішувала проблеми на веб-сайті',
+      'Розробила чистий код для клієнтського проєкту за допомогою React',
+      'Створела адаптивну взаємодію користувача з мобільними пристроями, яка дозволила міжплатформний доступ до веб-сайту на будь-якому пристрої з основним використанням на мобільному пристрої',
+      'Займалася перевіркою веб-сайту, щоб переконатися в функціональності',
+      'Використала Firebase Database, щоб дозволити користувачам надсилати інформацію, зберігаючи там дані',
+    ],
+  },
+  {
+    position: 'Вчитель німецької мови',
+    place:
+      'Otto Hans Language School, м. Івано-Франківськ, червень 2020 - по теперішній час',
+    description: [
+      'Навчаю дорослих студентів на рівнях А1-С1 німецької мови',
+      'Працюю над створенням і підтримкою позитивного навчального середовища',
+    ],
+  },
+  {
+    position: 'Репетитор',
+    place: 'Самозайнята, 2017 - по теперішній час',
+    description: [
+      'Склала плани уроків, що відповідають індивідуальним потребам учнів',
+      'Проводжу цілеспрямоване навчання для досягнення розуміння студентами матеріалу',
+    ],
+  },
+];
+
+export const softSkillsUkr = [
+  'Комунікативні навички',
+  'Критичне мислення',
+  'Увага до деталей',
+  'Ефективний тайм-менеджмент',
+  'Навички вирішення проблем',
+  'Креативність',
+  'Здатність працювати під тиском',
+];
+
+export const headingsUkr = [
+  'Контакти',
+  'Hard Skills',
+  'Soft Skills',
+  'Мови',
+  'Про себе',
+  'Освіта',
+  'Досвід роботи',
+  'Курси',
+  'Проєкти',
+];
+
+export const languagesUkr = ['Українська', 'Англійська', 'Німецька'];
